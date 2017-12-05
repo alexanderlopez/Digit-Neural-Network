8595cbac960e46a4b6d0c2db891c1c9ad0a7d7f7
Alexander Lopez <alexanderlopez00@gmail.com>
2017-12-04 22:27:05 -0400

    Update Matrix

--- src/main/Matrix.java
+++ src/main/Matrix.java
@@ -51,6 +51,32 @@ public class Matrix {
 		return transposed;
 	}
 	
+	public static Matrix multiply(Matrix A, double scalar) {
+		Matrix resultant = new Matrix(A.getRows(), A.getCols());
+		for (int i = 0; i < A.getRows(); i++)
+			for (int j = 0; j < A.getCols(); j++) {
+				double val = A.getValueAt(i, j)*scalar;
+				resultant.setValue(val, i, j);
+			}
+		
+		return resultant;
+	}
+	
+	public static Matrix add(Matrix A, Matrix B) {
+		if (A.getRows() != B.getRows() || A.getCols() != B.getCols())
+			return null;
+		
+		Matrix resultant = new Matrix(A.getRows(), A.getCols());
+		
+		for (int i = 0; i < A.getRows(); i++)
+			for (int j = 0; j < A.getCols(); j++) {
+				double sum = A.getValueAt(i, j) + B.getValueAt(i, j);
+				resultant.setValue(sum, i, j);
+			}
+		
+		return resultant;
+	}
+	
 	public static Matrix dot(Matrix A, Matrix B) {
 		if (A.getCols() != B.getRows())
 			return null;
@@ -59,9 +85,13 @@ public class Matrix {
 		
 		for (int i = 0; i < A.getRows(); i++) {
 			for (int j = 0; j < B.getCols(); j++) {
+				double sum = 0;
+				
 				for (int k = 0; k < A.getCols(); k++) {
-					
+					sum += A.getValueAt(i, k) * B.getValueAt(k, j);
 				}
+				
+				resultant.setValue(sum, i, j);
 			}
 		}
 		
